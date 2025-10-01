55d5c3de64f94405d189076bffd14411c40ad91a
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-01 23:13:58 +0530

    feat: implement InventoryController and InventoryService for event and venue data retrieval

---
+++ Inventory Service/src/main/java/com/sathwikhbhat/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,36 @@
+package com.sathwikhbhat.inventoryservice.controller;
+
+import com.sathwikhbhat.inventoryservice.response.EventInventoryResponse;
+import com.sathwikhbhat.inventoryservice.response.VenueInventoryResponse;
+import com.sathwikhbhat.inventoryservice.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class InventoryController {
+    
+    private InventoryService inventoryService;
+
+    @Autowired
+    public InventoryController(final InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping("/inventory/events")
+    public @ResponseBody List<EventInventoryResponse> inventoryGetAllEvents() {
+        return inventoryService.getAllEvents();
+    }
+
+    @GetMapping("/inventory/venue/{venueId}")
+    public @ResponseBody VenueInventoryResponse inventoryByVenueId(@PathVariable final Long venueId) {
+        return inventoryService.getVenueInformation(venueId);
+    }
+
+}
---
+++ Inventory Service/src/main/java/com/sathwikhbhat/inventoryservice/service/InventoryService.java
@@ -0,0 +1,44 @@
+package com.sathwikhbhat.inventoryservice.service;
+
+import com.sathwikhbhat.inventoryservice.entity.Event;
+import com.sathwikhbhat.inventoryservice.entity.Venue;
+import com.sathwikhbhat.inventoryservice.repository.EventRepository;
+import com.sathwikhbhat.inventoryservice.repository.VenueRepository;
+import com.sathwikhbhat.inventoryservice.response.EventInventoryResponse;
+import com.sathwikhbhat.inventoryservice.response.VenueInventoryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InventoryService {
+
+    private final EventRepository eventRepository;
+    private final VenueRepository venueRepository;
+
+    @Autowired
+    public InventoryService(final EventRepository eventRepository, final VenueRepository venueRepository) {
+        this.eventRepository = eventRepository;
+        this.venueRepository = venueRepository;
+    }
+
+    public List<EventInventoryResponse> getAllEvents() {
+        final List<Event> events = eventRepository.findAll();
+        return events.stream().map(event -> EventInventoryResponse.builder()
+                .event(event.getName())
+                .capacity(event.getLeftCapacity())
+                .venue(event.getVenue())
+                .build()).toList();
+    }
+
+    public List<VenueInventoryResponse> getVenueInformation(final Long venueId) {
+        final Venue venue = venueRepository.findById(venueId).orElse(null);
+        return List.of(VenueInventoryResponse.builder()
+                .venueId(venue.getId())
+                .venueName(venue.getName())
+                .totalCapacity(venue.getTotalCapacity())
+                .build());
+    }
+
+}

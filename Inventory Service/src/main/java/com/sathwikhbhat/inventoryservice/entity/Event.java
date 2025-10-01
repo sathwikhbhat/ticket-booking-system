074601b6f2fe6e608bbeb356e1d634111c13b132
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-01 22:54:52 +0530

    feat: add Event and Venue entity classes

---
+++ Inventory Service/src/main/java/com/sathwikhbhat/inventoryservice/entity/Event.java
@@ -0,0 +1,29 @@
+package com.sathwikhbhat.inventoryservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+
+    @Id
+    private Long id;
+    private String name;
+    private Long totalCapacity;
+    private Long leftCapacity;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_id", nullable = false)
+    private Venue venue;
+
+}
---
+++ Inventory Service/src/main/java/com/sathwikhbhat/inventoryservice/entity/Venue.java
@@ -0,0 +1,23 @@
+package com.sathwikhbhat.inventoryservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Venue {
+
+    @Id
+    private Long id;
+    private String name;
+    private String address;
+    private Long totalCapacity;
+
+}

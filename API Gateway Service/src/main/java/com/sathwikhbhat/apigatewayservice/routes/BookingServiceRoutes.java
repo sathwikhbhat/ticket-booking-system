324bdaa511341cd3b1dfe08d2ee3ec9bd67917ab
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-03 14:09:27 +0530

    feat: add BookingServiceRoutes for routing ticket booking requests

---
+++ API Gateway Service/src/main/java/com/sathwikhbhat/apigatewayservice/routes/BookingServiceRoutes.java
@@ -0,0 +1,22 @@
+package com.sathwikhbhat.apigatewayservice.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class BookingServiceRoutes {
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryRoutes() {
+        return GatewayRouterFunctions.route("inventory-service")
+                .route(RequestPredicates.POST("/api/v1/bookTickets"),
+                        HandlerFunctions.forward("http://localhost:8081/api/v1/booking"))
+                .build();
+    }
+
+}

f07cf9efb9b7b889961448d786189280eeca155d
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-03 15:56:03 +0530

    feat: add circuit breaker and fallback route for booking service

--- API Gateway Service/src/main/java/com/sathwikhbhat/apigatewayservice/routes/BookingServiceRoutes.java
+++ API Gateway Service/src/main/java/com/sathwikhbhat/apigatewayservice/routes/BookingServiceRoutes.java
@@ -1,12 +1,16 @@
 package com.sathwikhbhat.apigatewayservice.routes;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 @Configuration
 public class BookingServiceRoutes {
@@ -16,6 +20,17 @@ public class BookingServiceRoutes {
         return GatewayRouterFunctions.route("booking-service")
                 .route(RequestPredicates.POST("/api/v1/booking"),
                         HandlerFunctions.forward("http://localhost:8081/api/v1/booking"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRoute() {
+        return GatewayRouterFunctions.route("fallbackRoute")
+                .POST("/fallbackRoute",
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                .body("Booking Service is currently unavailable. Please try again later."))
                 .build();
     }
 

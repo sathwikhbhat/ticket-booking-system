f6370fb4882980af21b22b7f02742eab62e4edcd
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-03 13:16:37 +0530

    feat: add InventoryServiceClient for inter-service calls

---
+++ Order Service/src/main/java/com/sathwikhbhat/orderservice/client/InventoryServiceClient.java
@@ -0,0 +1,21 @@
+package com.sathwikhbhat.orderservice.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class InventoryServiceClient {
+
+    @Value("${inventory.service.url}")
+    private String inventoryServiceUrl;
+
+    public ResponseEntity<Void> updateInventory(final Long eventId,
+                                                final Long ticketsBooked) {
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.put(inventoryServiceUrl + "/event/" + eventId + "/capacity/" + ticketsBooked, null);
+        return ResponseEntity.ok().build();
+    }
+
+}

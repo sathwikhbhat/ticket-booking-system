e8a8894f0a14572ccc09d28c8f8721b2ed92505d
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2025-10-03 09:21:37 +0530

    feat: add Customer entity and repository for managing customer data

---
+++ Booking Service/src/main/java/com/sathwikhbhat/bookingservice/entity/Customer.java
@@ -0,0 +1,23 @@
+package com.sathwikhbhat.bookingservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    private Long id;
+    private String name;
+    private String email;
+    private String address;
+
+}
